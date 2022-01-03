627bee9198417d93ac87ea488ab074330f907d2b
Vo Tran Nhut Khanh <leekhanh1993@gmail.com>
2022-01-03 21:29:16 +0700

    finish add  bottom navigation bar

---
+++ app/src/main/java/com/example/assignment3/fragments/Home.java
@@ -0,0 +1,31 @@
+package com.example.assignment3.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.assignment3.R;
+
+public class Home extends Fragment {
+    public Home() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view  = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
